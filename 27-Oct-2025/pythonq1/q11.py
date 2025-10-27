c0fb0288e40bc68886ae92aa9dc47cc4b174a9f6
Himanshu Kumar <145198467+himanshukr0302@users.noreply.github.com>
2025-10-27 12:04:56 +0000

    new method to convert list to array

--- 27-Oct-2025/pythonq1/q11.py
+++ 27-Oct-2025/pythonq1/q11.py
@@ -2,8 +2,11 @@
 11.how to convert  a heterogenous data to homogenous data give an example ?
    (hint convert a list to an array)
 '''
-import numpy as np
-heterogeneous_list = [1, 2.5, '3', 4, '5.5']
-homogeneous_array = np.array(heterogeneous_list, dtype=float)
-print("Heterogeneous List:", heterogeneous_list)
-print("Homogeneous Array:", homogeneous_array)
+
+import array
+
+lst = [1, "2", 3.0, 4]         
+int_arr = array.array('i', map(int, lst))
+
+lst = int_arr.tolist()
+print(lst)
